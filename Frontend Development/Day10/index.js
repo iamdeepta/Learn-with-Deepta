408b712657cccca4ed97cc2b0efedcf0b85926ca
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2023-10-13 23:12:01 +0600

    javaScript with primitive and non primitive data types

---
+++ Frontend Development/Day10/index.js
@@ -0,0 +1,56 @@
+var x = 5;
+
+var y = 4;
+
+let name = "Anik"; //string
+let isString = true; //boolean
+
+let array = ["abc", "def", "ghi"];
+
+//object
+let player = {
+  name: "Sakib",
+  salary: 100000,
+  age: 36,
+  team: [
+    {
+      name: "Tamim",
+      age: 42,
+    },
+    {
+      name: "Soummo",
+      age: 35,
+    },
+    { name: "Taskin", age: 38 },
+  ],
+};
+
+console.log(player.name, player.team);
+
+// console.log(array.length);
+
+// console.log(x + y);
+
+//primitive data type
+// number - 1,2,3
+// string - rahim, korim, anik, ball, bat
+// boolean - true/false
+// float - 1.20, 4.56
+
+//non primitive data type
+// array - ["alu", "murgi", "anik", "deepta"] (array of string)
+// [1, 2, 3, 4, 5] (array of number)
+
+// object - 2 things - key, value
+
+// {
+//     name: "Tiger",
+//     leg: 4,
+//     city: "Dhaka",
+//     food: ["chicken", "duck"],
+// }
+
+// {
+//     name: "Anik",
+//     classRoom: [1, 2, 3, 4]
+// }
